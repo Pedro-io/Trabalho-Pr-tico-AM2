92e02f352292632efea84d36ccfbd63af47df256
Pedro-io <p.rodrigues2532@gmail.com>
2024-11-28 22:50:21 -0300

    Tratmento de outiliers. inicio da construção das analises fatoriais.

---
+++ src/pre_process/Discretização.py
@@ -0,0 +1,13 @@
+#%% 
+import pandas as pd 
+import numpy as np 
+
+
+#%%
+# Configurando e Carregando nossos dados
+# Permite visualizar todas as colunas do DataFrame
+pd.set_option('display.max_columns', None)
+
+# Leitura dos dados
+df = pd.read_csv(r"C:\Users\maype\Desktop\projetos\Trabalho Prático AM2\data\base_formatada.csv")
+# %%
